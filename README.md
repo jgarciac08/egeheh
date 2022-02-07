0362c5f79a0a3a7ca5e80754ca5c35215586bb9c
jgarciac08 <jgarciac08@educantabria.es>
2022-02-07 12:21:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-d1y6zg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-d1y6zg)
---
+++ src/components/BarraNavegacion.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class BarraNavegacion extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          <li>
+            <a href="#ej1">Ejercicio 1</a>
+          </li>
+          <li>
+            <a href="#ej2">Ejercicio 2</a>
+          </li>
+          <li>
+            <a href="#ej3">Ejercicio 3</a>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
+export default BarraNavegacion;
